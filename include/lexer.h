aea1c64908c7097b40c9650100c3883eb1184d0d
Kaleb Szaro <102062436+kkaotic@users.noreply.github.com>
2024-01-08 15:48:05 -0500

    Update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -1,1 +1,27 @@
+#pragma once
+#include "piping.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
 
+typedef struct {
+    char ** items;
+    size_t size;
+} tokenlist;
+
+
+char * get_input(void);
+char *path_Search(char* tokens);
+tokenlist * get_tokens(char *input);
+tokenlist * new_tokenlist(void);
+char * path_Search(char* tokens);
+char *get_outputfile(char *input);
+char *get_inputfile(char *input);
+char *expandEnv(char *tok);
+void add_token(tokenlist *tokens, char *item);
+void free_tokens(tokenlist *tokens);
